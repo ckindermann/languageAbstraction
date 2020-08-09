0301859b24a3fcf2801f80e38cb608e2c177e2a2
chris <chris.kind.man@gmail.com>
2020-08-09 23:41:59 +0100

    Add ground generalisation experiment

---
+++ src/main/java/uk/ac/man/cs/exp/AxiomGroundGeneralisation.java
@@ -0,0 +1,72 @@
+package uk.ac.man.cs.exp;
+
+
+import uk.ac.man.cs.ont.*;
+import uk.ac.man.cs.util.*;
+import uk.ac.man.cs.regularities.*;
+import uk.ac.man.cs.parser.*;
+import uk.ac.man.cs.structure.*;
+import uk.ac.man.cs.structure.nodes.*;
+import uk.ac.man.cs.iso.gg.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import org.semanticweb.owlapi.util.*;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.parameters.Imports;
+import org.semanticweb.owlapi.model.parameters.*;
+import org.semanticweb.owlapi.formats.OWLXMLDocumentFormat;
+import org.semanticweb.owlapi.formats.RDFXMLDocumentFormat;
+import org.semanticweb.owlapi.model.*;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+import org.semanticweb.owlapi.reasoner.*;
+import org.semanticweb.HermiT.ReasonerFactory;
+import java.util.logging.Logger;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.*;
+import org.semanticweb.elk.owlapi.ElkReasonerFactory;
+import org.semanticweb.owlapi.change.AxiomChangeData;
+import org.semanticweb.owlapi.search.EntitySearcher; 
+
+import java.nio.file.*;
+
+import uk.ac.manchester.cs.owlapi.modularity.SyntacticLocalityModuleExtractor;
+import uk.ac.manchester.cs.owlapi.modularity.ModuleType;
+
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.traverse.*;
+
+/**
+ * A class to demonstrate the functionality of the library.
+ */
+public class AxiomGroundGeneralisation {
+
+    private static final Logger log = Logger.getLogger(String.valueOf(AxiomGroundGeneralisation.class));
+
+    public static void main(String[] args) throws IOException , Exception{
+
+        String ontFilePath = args[0]; 
+        String output = args[1]; 
+
+        File ontFile = new File(ontFilePath);
+        log.info("\tLoading Ontology : " + ontFile.getName()); 
+        OntologyLoader ontLoader = new OntologyLoader(ontFile, true);
+        OWLOntology ont = ontLoader.getOntology(); 
+
+        String ontologyName = Paths.get(ontFilePath).getFileName().toString();
+
+        MyTimer timer = new MyTimer();
+        timer.go();
+        GroundGeneralisations ggs= new GroundGeneralisations(ont);
+        log.info(timer.stop("GG " + ontologyName));
+
+        int regularities = ggs.getRegularity2instance().size();
+        System.out.println("size : " + ggs.getRegularity2instance().size());
+        IOHelper.writeAppend(ontologyName + "," + regularities, output + "/groundGeneralisation"); 
+    }
+}
