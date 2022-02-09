f8203fe549466907d952accf83a1f7549c53af44
chris <chris.kind.man@gmail.com>
2022-02-09 21:06:47 +0000

    Add stub for AxiomTypeGeneralisation experiment

---
+++ src/main/java/uk/ac/man/cs/exp/axiomtypesets/AxiomTypeGroundGeneralisation.java
@@ -0,0 +1,72 @@
+package uk.ac.man.cs.exp.axiomtypesets;
+
+
+import uk.ac.man.cs.ont.*;
+import uk.ac.man.cs.util.*;
+import uk.ac.man.cs.regularities.axiomtypesets.gg.*;
+import uk.ac.man.cs.parser.*;
+import uk.ac.man.cs.structure.*;
+import uk.ac.man.cs.structure.nodes.*;
+import uk.ac.man.cs.iso.renaming.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import org.semanticweb.owlapi.util.*;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.parameters.Imports;
+import org.semanticweb.owlapi.model.parameters.*;
+import org.semanticweb.owlapi.formats.OWLXMLDocumentFormat;
+import org.semanticweb.owlapi.formats.RDFXMLDocumentFormat;
+import org.semanticweb.owlapi.model.*;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+import org.semanticweb.owlapi.reasoner.*;
+import org.semanticweb.HermiT.ReasonerFactory;
+import java.util.logging.Logger;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.*;
+import org.semanticweb.elk.owlapi.ElkReasonerFactory;
+import org.semanticweb.owlapi.change.AxiomChangeData;
+import org.semanticweb.owlapi.search.EntitySearcher; 
+
+import java.nio.file.*;
+
+import uk.ac.manchester.cs.owlapi.modularity.SyntacticLocalityModuleExtractor;
+import uk.ac.manchester.cs.owlapi.modularity.ModuleType;
+
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.traverse.*;
+
+/**
+ * A class to demonstrate the functionality of the library.
+ */
+public class AxiomTypeGroundGeneralisation {
+
+    private static final Logger log = Logger.getLogger(String.valueOf(AxiomTypeGroundGeneralisation.class));
+
+    public static void main(String[] args) throws IOException , Exception{
+
+        String ontFilePath = args[0]; 
+        String output = args[1]; 
+
+        File ontFile = new File(ontFilePath);
+        //log.info("\tLoading Ontology : " + ontFile.getName()); 
+        OntologyLoader ontLoader = new OntologyLoader(ontFile, true);
+        OWLOntology ont = ontLoader.getOntology(); 
+
+        String ontologyName = Paths.get(ontFilePath).getFileName().toString();
+
+        MyTimer timer = new MyTimer();
+        timer.go();
+        WeightedClassFrameGGMiner miner = new WeightedClassFrameGGMiner(ont);
+        log.info(timer.stop("GG for " + ontologyName));
+
+        int regularities = miner.getRegularity2instance().size();
+        //System.out.println("size : " + miner.getRegularity2instance().size());
+        IOHelper.writeAppend(ontologyName + "," + regularities, output + "/groundGeneralisation"); 
+    }
+}
